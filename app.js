e1d7dade52b11aec4017e30e0964a712d53088e2
attaulrehman100 <mujeebleghari45@gmail.com>
2025-07-18 17:56:11 -0700

    Js Asssignment 2

---
+++ app.js
@@ -0,0 +1,73 @@
+
+                        //     chapter 7
+                        // qno1
+
+//       var CityName = prompt("Enter your City Name:")
+
+
+// alert("Welcome to city of lights: " )
+
+      //      qno2
+
+
+//       var gender = prompt("Please enter your gender (male/female):");
+   
+//       if(gender === "male"){
+//            alert("Good Morning sir " );
+//       } else if (gender === "female"){
+//                 alert("Good Morning Ma'am ")
+//       } else {
+//          alert("Invalid input. Please enter 'male' or 'female'.");
+// }
+
+//    qno 3
+
+// var color = prompt("take input color of road traffic signal (Red/Yellow/Green):");
+// if(color === "Red"){
+//       alert("Must Stop");
+// } else if (color === "Yellow") {
+//       alert("Ready to move")
+// } else if (color === "Green") {
+//       alert("Move now")
+// } else {
+//      alert ("Invalid input. ");
+// }
+
+                              //   chapter  08
+
+      // qno1
+
+
+//       let char = prompt("Enter a single character:");
+
+// if (char.length === 1) {
+//     let code = char.charCodeAt(0);
+
+//     if (code >= 48 && code <= 57) {
+//         alert("It is a number.");
+//     } else if (code >= 65 && code <= 90) {
+//         alert("It is an uppercase letter.");
+//     } else if (code >= 97 && code <= 122) {
+//         alert("It is a lowercase letter.");
+//     } else {
+//         alert("It is a special character or symbol.");
+//     }
+// } else {
+//     alert("Please enter only one character.");
+// }
+
+// qno 3
+
+
+// let number = prompt("Enter a number:");
+// number = Number(number); // Convert input to a number
+
+// if (number > 0) {
+//     alert("The number is positive.");
+// } else if (number < 0) {
+//     alert("The number is negative.");
+// } else if (number === 0) {
+//     alert("The number is zero.");
+// } else {
+//     alert("Invalid input. Please enter a valid number.");
+// }
